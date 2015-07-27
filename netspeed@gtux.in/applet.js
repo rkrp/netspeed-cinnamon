3f0a49d742fd05ba46a38fb20056a3ec62a53ff3
rkrp <krp@gtux.in>
2015-07-27 11:13:13 +0530

    Added Upload speed monitor

--- netspeed@gtux.in/applet.js
+++ netspeed@gtux.in/applet.js
@@ -14,9 +14,15 @@ MyApplet.prototype = {
     title: 0,
 
     old_rx : 0,
+    old_tx : 0,
 
+    //Download data provider
     rx_path_template: "/sys/class/net/%s/statistics/rx_bytes",
     rx_path: "",
+
+    //Upload data provider
+    tx_path_template: "/sys/class/net/%s/statistics/tx_bytes",
+    tx_path: "",
 
     _init: function(orientation, panel_height, instance_id) {
         Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
@@ -29,37 +35,56 @@ MyApplet.prototype = {
         //Option to select network iface
         var iface = "wlp1s0";
         this.rx_path = this.rx_path_template.replace("%s", iface);
+        this.tx_path = this.tx_path_template.replace("%s", iface);
     
         this._update_speed();
     },
 
     _update_speed: function() {
-        this._get_speed();
-        this.set_applet_label(this.title);
+        let down_speed = this._get_down_speed();
+        let up_speed = this._get_up_speed();
+        this.set_applet_label(down_speed + " KB/s ▼ " + up_speed + " KB/s ▲");
 
         //Use MainLoop to run this function every 1000 ms
         Mainloop.timeout_add(1000, Lang.bind(this, this._update_speed));
     },
 
-    _get_speed: function(iface) {
-        var speed = GLib.file_get_contents(this.rx_path);
+    _get_down_speed: function(iface) {
+        var bytes_received = GLib.file_get_contents(this.rx_path);
 
-        if(speed[0]) {
-            var new_rx = parseInt(speed[1]);
+        if(bytes_received[0]) {
+            var new_rx = parseInt(bytes_received[1]);
 
             //toFixed() returns a string. Otherwise, must use toString()
-            this.title = "▼ " + ((new_rx - this.old_rx)/1024).toFixed(2) + " KB/s";
+            var current_speed = ((new_rx - this.old_rx)/1024).toFixed(2);
 
             //Save the current value in old_rx
             this.old_rx = new_rx;
         }
 
-        GLib.free(speed);
-        
+        GLib.free(bytes_received);
+        return current_speed;
+    },
+
+    _get_up_speed: function(iface) {
+        var bytes_sent = GLib.file_get_contents(this.tx_path);
+
+        if(bytes_sent[0]) {
+            let new_tx = parseInt(bytes_sent[1]);
+
+            //toFixed() returns a string. Otherwise, must use toString()
+            var current_speed = ((new_tx - this.old_tx)/1024).toFixed(2);
+
+            //Save the current value in old_tx
+            this.old_tx = new_tx;
+        }
+
+        GLib.free(bytes_sent);
+        return current_speed;
     },
     
     on_applet_clicked: function() {
-        Util.spawn('dasdas');
+        
     }
 };
 
